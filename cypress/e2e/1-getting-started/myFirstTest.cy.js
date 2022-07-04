91179dfe5ea2bcb9109a664adacbe8ad451082b3
Ashwini Karai <ashwinikarai@AshwiniaraisMBP.localdomain.lan>
2022-07-03 21:12:00 -0600

    first test to locate elements on https://example.cypress.io/commands/actions page

---
+++ cypress/e2e/1-getting-started/myFirstTest.cy.js
@@ -0,0 +1,28 @@
+
+context("my first test", () => {
+   beforeEach(() => {
+        cy.visit("https://example.cypress.io/commands/actions")
+   })
+
+   it("has h1 attribute with correct text", () => {
+      cy.get('h1')
+      .should('exist')
+      .should('contain.text', 'Actions')
+   })
+
+   it("renders a paragraph below the h1", () => {
+      //the paragraph can be located via its immediate parent which is associated with a .container class
+      //the parent's css locator .container class matches several elements on the page, so we zero in on the second one with .eq(1)
+      cy.get('.container').eq(1)
+      .find('p').should('exist')
+   })
+
+   it("renders a section with the correct elements", () => {
+      cy.get('.container').eq(2).within(() => {
+         //there are many elements within the .container element so we can limit scope within that container
+         cy.get('h4').should('contain.text', 'type()')
+         cy.get('p').should('exist')
+         cy.get('pre').should('exist')
+      })
+   })
+})
