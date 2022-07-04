dc794fb3dde88b087519f835350690c21bd4171a
Ashwini Karai <ashwinikarai@AshwiniaraisMBP.localdomain.lan>
2022-07-03 21:19:23 -0600

    install and import the cypress testing library plugin

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+//Added by Ash as per https://github.com/testing-library/cypress-testing-library
+import '@testing-library/cypress/add-commands'
