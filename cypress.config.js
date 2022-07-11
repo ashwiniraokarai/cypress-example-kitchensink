d3a062330df83dc69665b9f3f5e21d21c72fca40
Ashwini Karai <ashwinikarai@AshwiniaraisMBP.localdomain.lan>
2022-07-11 15:12:26 -0600

    point baseUrl to localhost:8080 and see if CI picks it up

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   'projectId': '4b7344',
   e2e: {
-    baseUrl: "https://example.cypress.io"
-    //baseUrl: "http://localhost:8080"
+    //baseUrl: "https://example.cypress.io"
+    baseUrl: "http://localhost:8080" //want CI to test against local server
   },
   env: {
     navBarText: "cypress.io"
